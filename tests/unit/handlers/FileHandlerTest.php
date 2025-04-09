7980b77427f4098ba2a58eec2a467352c8b188be
msamgan <mohdsamgankhan@gmail.com>
2025-04-09 17:32:04 -0400

    test

--- tests/unit/handlers/FileHandlerTest.php
+++ tests/unit/handlers/FileHandlerTest.php
@@ -20,10 +20,20 @@ it('checks for ensureJsFileExists', function () {
 
 it('checks for appendToFileWithEmptyLine', function () {
     $filePath = $this->fileHandler->ensureJsFileExists(fileName: 'UserController');
-    $this->fileHandler->appendToFileWithEmptyLine(
-        filePath: $filePath,
-        content: 'Content Test',
-    );
-
+    $this->fileHandler->appendToFileWithEmptyLine(filePath: $filePath, content: 'Content Test');
     expect(file_get_contents($filePath))->toBe('// Action file: UserController' . PHP_EOL . 'Content Test' . PHP_EOL);
+
+    $filePath = $this->fileHandler->ensureJsFileExists(fileName: 'AdminController');
+    $this->fileHandler->appendToFileWithEmptyLine(filePath: $filePath, content: 'Content Test 2');
+    expect(file_get_contents($filePath))->toBe('// Action file: AdminController' . PHP_EOL . 'Content Test 2' . PHP_EOL);
+});
+
+it ('checks for removeDirectoryRecursively', function () {
+    $this->fileHandler->ensureJsFileExists(fileName: 'UserController');
+    $this->fileHandler->removeDirectoryRecursively();
+    expect(is_dir($this->fileHandler->currentResourcePath($this->fileHandler->getPrefix())))->toBeFalse();
+
+    $this->fileHandler->ensureJsFileExists(fileName: 'ProfileController', filePath: implode('/', ['Settings']));
+    $this->fileHandler->removeDirectoryRecursively();
+    expect(is_dir($this->fileHandler->currentResourcePath($this->fileHandler->getPrefix())))->toBeFalse();
 });
