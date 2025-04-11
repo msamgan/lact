7c8a05889f5888733daab059a42ae53dbbf87e27
msamgan <mohdsamgankhan@gmail.com>
2025-04-11 16:31:36 -0400

    added default method and path option.

--- routes/lact.php
+++ routes/lact.php
@@ -1,11 +1,8 @@
 <?php
 
-declare(strict_types=1);
+Route::post('025d7c1e-4ffb-458d-9e79-848c05ddd119', [App\Http\Controllers\Settings\ProfileController::class, 'updateData'])->name('profile.update.data')->middleware(['web'])->prefix('action');
 
-// test URL will be removed after installation.
-Route::post(
-    '85906367-216d-4f44-b463-4c3508478b52/{user}',
-    [App\Http\Controllers\LactTestController::class, 'testFunction']
-)->name('test.function')
-    ->middleware(['auth', 'verified'])
-    ->prefix('action');
+Route::get('d12a77d2-05ea-4cdb-9ce7-b7790e480826', [App\Http\Controllers\Settings\ProfileController::class, 'getUserData'])->name('profile.get.user.data')->middleware(['web'])->prefix('action');
+
+Route::post('user/update/{user}', [App\Http\Controllers\DashboardController::class, 'updateUser'])->name('user.update')->middleware(['web', 'auth', 'verified'])->prefix('action');
+
