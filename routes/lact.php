5bcde98dbbfc1cd8503e6eeafb563c8cf808d5a8
msamgan <29948727+msamgan@users.noreply.github.com>
2025-04-11 20:31:54 +0000

    Fix styling

--- routes/lact.php
+++ routes/lact.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 Route::post('025d7c1e-4ffb-458d-9e79-848c05ddd119', [App\Http\Controllers\Settings\ProfileController::class, 'updateData'])->name('profile.update.data')->middleware(['web'])->prefix('action');
 
 Route::get('d12a77d2-05ea-4cdb-9ce7-b7790e480826', [App\Http\Controllers\Settings\ProfileController::class, 'getUserData'])->name('profile.get.user.data')->middleware(['web'])->prefix('action');
 
 Route::post('user/update/{user}', [App\Http\Controllers\DashboardController::class, 'updateUser'])->name('user.update')->middleware(['web', 'auth', 'verified'])->prefix('action');
-
