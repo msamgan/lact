36c7fb92808ab5ecd41ee44b22bd84bf9b40f64e
msamgan <mohdsamgankhan@gmail.com>
2025-04-15 14:54:11 -0400

    updated command.

---
+++ resources/methods.js
@@ -0,0 +1,10 @@
+import { execSync } from 'node:child_process'
+
+export const  lactPreBuild = () => {
+    return {
+        name: 'lact-pre-build',
+        async buildStart() {
+            execSync('php artisan lact:run', { stdio: 'inherit' })
+        },
+    }
+}
